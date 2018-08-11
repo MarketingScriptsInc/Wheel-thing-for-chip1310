8978196e42bcd5d8db9cf35bae232eed811c746f
Geocube333 <Geocube333@gmail.com>
2018-08-11 03:11:05 -0400

    Wheel-thing 0.0.2
    Made it somewhat visually appealing, the next update will make it more user-friendly, hopefully adding a menu that could allow a user to make more parts of the wheel to land on.

--- Wheel-thing.js
+++ Wheel-thing.js
@@ -7,21 +7,21 @@ function xyz() {
   //This is what gives you the questions it picks a random number between 1 and 360
 
   document.getElementById('Wheel').style.transform = 'rotate(' + spin + 'deg)';
-  document.getElementById('Square').innerHTML = '<p id="Button" onclick="xyz()"> '+ spin +' </p>'
+  document.getElementById('Square').innerHTML = '<p id="Button" onclick="xyz()"> '+ spin +' </p>';
 
   //These just change the css stuff:
   //The first one takes the random number from spin and rotates the wheel div element (in Wheel-thing.html)
   //The second one just displays the spin value (or technically the angle), that could be deleted or commented out
   //(With //) 
 
-  if (spin <= 45 && spin > 0) {alert('question 1');} 
-  else if (spin <= 90 && spin > 45) {alert('question 2');}
-  else if (spin <= 135 && spin > 90) {alert('question 3')}
-  else if (spin <= 180 && spin > 135) {alert('question 4')}
-  else if (spin <= 225 && spin > 180) {alert('question 5')}
-  else if (spin <= 270 && spin > 225) {alert('question 6')}
-  else if (spin <= 315 && spin > 270) {alert('question 7')}
-  else if (spin <= 360 && spin > 315) {alert('question 8')}
+  if (spin <= 45 && spin > 0) {document.getElementById('Questions').innerHTML = 'Question 1';} 
+  else if (spin <= 90 && spin > 45) {document.getElementById('Questions').innerHTML = 'Question 2';}
+  else if (spin <= 135 && spin > 90) {document.getElementById('Questions').innerHTML = 'Question 3';}
+  else if (spin <= 180 && spin > 135) {document.getElementById('Questions').innerHTML = 'Question 4';}
+  else if (spin <= 225 && spin > 180) {document.getElementById('Questions').innerHTML = 'Question 5';}
+  else if (spin <= 270 && spin > 225) {document.getElementById('Questions').innerHTML = 'Question 6';}
+  else if (spin <= 315 && spin > 270) {document.getElementById('Questions').innerHTML = 'Question 7';}
+  else if (spin <= 360 && spin > 315) {document.getElementById('Questions').innerHTML = 'Question 8';}
 
   //All of the if statements are the different questions, to add more questions add more if statements
   //ALL IF STATEMENTS MUST NOT GO OVER 360, it breaks things and it gets annoying
